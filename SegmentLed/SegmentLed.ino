5d1de54b4ae04fa3a1af16e418456de61feb5c83
Luc Oostdijk <l.oostdijk@student.tue.nl>
2018-10-03 17:04:58 +0200

    blocks implemented in working fashion

--- SegmentLed/SegmentLed.ino
+++ SegmentLed/SegmentLed.ino
@@ -8,12 +8,7 @@ const uint16_t PixelCount = 56; // make sure to set this to the number of pixels
 const uint8_t PixelPin = 22;  // make sure to set this to the correct pin, ignored for Esp8266
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
-// For Esp8266, the Pin is omitted and it uses GPIO3 due to DMA hardware use.  
-// There are other Esp8266 alternative methods that provide more pin options, but also have
-// other side effects.
-//NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
-//
-// NeoEsp8266Uart800KbpsMethod uses GPI02 instead
+// Start NeoPixelBus
 
 
 // NeoPixel animation time management object
@@ -41,7 +36,7 @@ NeoPixelAnimator animations(PixelCount, NEO_CENTISECONDS);
 
 struct MyAnimationState
 {
-    RgbColor StartingColor = RgbColor(255,0,0);  // the color the animation starts at
+    RgbColor StartingColor = RgbColor(255,0,0);  // the color the animation starts at (RGBColor is called, so red-green-blue 0-255)
     RgbColor EndingColor = RgbColor(0,255,255); // the color the animation will end at
     AnimEaseFunction Easeing; // the acceleration curve it will use 
 };
@@ -70,6 +65,7 @@ void AnimUpdate(const AnimationParam& param)
 
 void SetRandomSeed()
 {
+    // Basically make a proper random. Called in setup
     uint32_t seed;
 
     // random works best with a seed that can use 31 bits
@@ -95,7 +91,7 @@ void setup()
     strip.Begin();
     strip.Show();
 
-    SetRandomSeed();
+    SetRandomSeed();  // get proper random numbers when calling random()
 
     // just pick some colors
     for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
@@ -111,38 +107,63 @@ void setup()
 
 void SetupAnimationSet()
 {
-    // setup some animations
-    for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
-    {
-        const uint8_t peak = 128;
+    const uint8_t peak = 255; // Max brightness value for a pixel
 
-        // pick a random duration of the animation for this pixel
-        // since values are centiseconds, the range is 1 - 4 seconds
-        uint16_t time = random(100, 400);
+    // each animation starts with the color that was present
+    RgbColor startingColor1 = RgbColor(peak, 0, 0);  // 1st set of blocks
+    RgbColor startingColor2 = RgbColor(0, peak, 0);  // 2nd set of blocks
+    
+    // and ends with a random color
+    RgbColor targetColor1 = RgbColor(0, peak, 0);
+    RgbColor targetColor2 = RgbColor(peak, 0, 0);
+    
+    // with the random ease function
+    AnimEaseFunction easing;
+    easing = NeoEase::QuadraticInOut;
 
-        // each animation starts with the color that was present
-        RgbColor originalColor = RgbColor(255,0,0);
-        // and ends with a random color
-        RgbColor targetColor = RgbColor(0,255,255);
-        // with the random ease function
-        AnimEaseFunction easing;
-        easing = NeoEase::QuadraticInOut;
+//        // each animation starts with the color that was present
+//        RgbColor originalColor = strip.GetPixelColor(pixel);
+//        // and ends with a random color
+//        RgbColor targetColor = RgbColor(random(peak), random(peak), random(peak));
+//        // with the random ease function
+//        AnimEaseFunction easing;
 
+    int BlockSet1[] = {0, 1, 2, 3, 4, 5, 6, 7, 16, 17, 18, 19, 20, 21, 22, 23, 32, 33, 34, 35, 36, 37, 38, 39, 48, 49, 50, 51, 52, 53, 54, 55};
+    int BlockSet2[] = {8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31, 40, 41, 42, 43, 44, 45, 46, 47};
 
+    // Block 1 animation setup 
+    for (uint16_t i = 0; i < (sizeof(BlockSet1)/sizeof(int)); i++)
+    { 
+        uint16_t pixel = BlockSet1[i];
+        
+        uint16_t time = random(700, 8000);  // Random duration of animation. Value in centiseconds. 100 = 1sec.
 
-
-        // each animation starts with the color that was present
-        animationState[pixel].StartingColor = originalColor;
-        // and ends with a random color
-        animationState[pixel].EndingColor = targetColor;
-        // using the specific curve
-        animationState[pixel].Easeing = easing;
+        animationState[pixel].StartingColor = startingColor1;  
+        animationState[pixel].EndingColor = targetColor1;
+        animationState[pixel].Easeing = easing;  // From startingColor to EndingColor using this specific curve
 
         // now use the animation state we just calculated and start the animation
         // which will continue to run and call the update function until it completes
         animations.StartAnimation(pixel, time, AnimUpdate);
-
     }
+
+    // Block 2 animation setup
+    for (uint16_t i = 0; i < (sizeof(BlockSet2)/sizeof(int)); i++)
+    { 
+        uint16_t pixel = BlockSet2[i];
+        
+        uint16_t time = random(700, 8000);  // Random duration of animation. Value in centiseconds. 100 = 1sec.
+
+        animationState[pixel].StartingColor = startingColor2;  
+        animationState[pixel].EndingColor = targetColor2;
+        animationState[pixel].Easeing = easing;  // From startingColor to EndingColor using this specific curve
+
+        // now use the animation state we just calculated and start the animation
+        // which will continue to run and call the update function until it completes
+        animations.StartAnimation(pixel, time, AnimUpdate);
+    }
+
+    
 }
 
 void loop()
@@ -162,6 +183,173 @@ void loop()
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// Easing function / red to blue
+//
+//#include <NeoPixelBus.h>
+//#include <NeoPixelAnimator.h>
+//
+//const uint16_t PixelCount = 56; // make sure to set this to the number of pixels in your strip
+//const uint8_t PixelPin = 22;  // make sure to set this to the correct pin, ignored for Esp8266
+//
+//NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+//// For Esp8266, the Pin is omitted and it uses GPIO3 due to DMA hardware use.  
+//// There are other Esp8266 alternative methods that provide more pin options, but also have
+//// other side effects.
+////NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
+////
+//// NeoEsp8266Uart800KbpsMethod uses GPI02 instead
+//
+//
+//// NeoPixel animation time management object
+//NeoPixelAnimator animations(PixelCount, NEO_CENTISECONDS);
+//
+//// create with enough animations to have one per pixel, depending on the animation
+//// effect, you may need more or less.
+////
+//// since the normal animation time range is only about 65 seconds, by passing timescale value
+//// to the NeoPixelAnimator constructor we can increase the time range, but we also increase
+//// the time between the animation updates.   
+//// NEO_CENTISECONDS will update the animations every 100th of a second rather than the default
+//// of a 1000th of a second, but the time range will now extend from about 65 seconds to about
+//// 10.9 minutes.  But you must remember that the values passed to StartAnimations are now 
+//// in centiseconds.
+////
+//// Possible values from 1 to 32768, and there some helpful constants defined as...
+//// NEO_MILLISECONDS        1    // ~65 seconds max duration, ms updates
+//// NEO_CENTISECONDS       10    // ~10.9 minutes max duration, centisecond updates
+//// NEO_DECISECONDS       100    // ~1.8 hours max duration, decisecond updates
+//// NEO_SECONDS          1000    // ~18.2 hours max duration, second updates
+//// NEO_DECASECONDS     10000    // ~7.5 days, 10 second updates
+////
+//
+//
+//struct MyAnimationState
+//{
+//    RgbColor StartingColor = RgbColor(255,0,0);  // the color the animation starts at
+//    RgbColor EndingColor = RgbColor(0,255,255); // the color the animation will end at
+//    AnimEaseFunction Easeing; // the acceleration curve it will use 
+//};
+//
+//MyAnimationState animationState[PixelCount];
+//// one entry per pixel to match the animation timing manager
+//
+//void AnimUpdate(const AnimationParam& param)
+//{
+//    // first apply an easing (curve) to the animation
+//    // this simulates acceleration to the effect
+//    float progress = NeoEase::ExponentialInOut(param.progress);
+//
+//    // this gets called for each animation on every time step
+//    // progress will start at 0.0 and end at 1.0
+//    // we use the blend function on the RgbColor to mix
+//    // color based on the progress given to us in the animation
+//    RgbColor updatedColor = RgbColor::LinearBlend(
+//        animationState[param.index].StartingColor,
+//        animationState[param.index].EndingColor,
+//        progress);
+//    // apply the color to the strip
+//    strip.SetPixelColor(param.index, updatedColor);
+//}
+//
+//
+//void SetRandomSeed()
+//{
+//    uint32_t seed;
+//
+//    // random works best with a seed that can use 31 bits
+//    // analogRead on a unconnected pin tends toward less than four bits
+//    seed = analogRead(0);
+//    delay(1);
+//
+//    for (int shifts = 3; shifts < 31; shifts += 3)
+//    {
+//        seed ^= analogRead(0) << shifts;
+//        delay(1);
+//    }
+//
+//    // Serial.println(seed);
+//    randomSeed(seed);
+//}
+//
+//void setup()
+//{
+//    Serial.begin(115200);
+//    while (!Serial); // wait for serial attach
+//
+//    strip.Begin();
+//    strip.Show();
+//
+//    SetRandomSeed();
+//
+//    // just pick some colors
+//    for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
+//    {
+//        RgbColor color = RgbColor(random(255), random(255), random(255));
+//        strip.SetPixelColor(pixel, color);
+//    }
+//
+//    Serial.println();
+//    Serial.println("Running...");
+//}
+//
+//
+//void SetupAnimationSet()
+//{
+//    // setup some animations
+//    for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
+//    {
+//        const uint8_t peak = 128;
+//
+//        // pick a random duration of the animation for this pixel
+//        // since values are centiseconds, the range is 1 - 4 seconds
+//        uint16_t time = random(100, 400);
+//
+//        // each animation starts with the color that was present
+//        RgbColor originalColor = RgbColor(255,0,0);
+//        // and ends with a random color
+//        RgbColor targetColor = RgbColor(0,255,255);
+//        // with the random ease function
+//        AnimEaseFunction easing;
+//        easing = NeoEase::QuadraticInOut;
+//
+//
+//
+//
+//        // each animation starts with the color that was present
+//        animationState[pixel].StartingColor = originalColor;
+//        // and ends with a random color
+//        animationState[pixel].EndingColor = targetColor;
+//        // using the specific curve
+//        animationState[pixel].Easeing = easing;
+//
+//        // now use the animation state we just calculated and start the animation
+//        // which will continue to run and call the update function until it completes
+//        animations.StartAnimation(pixel, time, AnimUpdate);
+//
+//    }
+//}
+//
+//void loop()
+//{
+//    if (animations.IsAnimating())
+//    {
+//        // the normal loop just needs these two to run the active animations
+//        animations.UpdateAnimations();
+//        strip.Show();
+//    }
+//    else
+//    {
+//        Serial.println();
+//        Serial.println("Setup Next Set...");
+//        // example function that sets up some animations
+//        SetupAnimationSet();
+//    }
+//}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
